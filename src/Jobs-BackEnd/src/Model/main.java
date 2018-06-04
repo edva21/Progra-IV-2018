4bd79af81c8a48ec7be293aa6ce925df64eb585d
edva590112 <edva590112@gmail.com>
2018-06-04 11:36:34 -0600

    23/05/2018 commit

---
+++ src/Jobs-BackEnd/src/Model/main.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+
+import BussinessLogic.Administrador;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import BussinessLogic.Empresa;
+import BussinessLogic.Habilidad_Porcentaje;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author gaspa
+ */
+public class main {
+    public static void main(String[] args) throws URISyntaxException, IOException, Exception {   
+                
+    
+    }
+    
+    
+}
